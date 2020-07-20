7728163b67e7157e771e0a55adf6cb2baeaa6693
MohauNkepane <45736432+MohauNkepane@users.noreply.github.com>
2020-07-20 14:18:21 +0200

    Add Comments to myApi1

--- myApi1.py
+++ myApi1.py
@@ -4,8 +4,10 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-Data = []
 
+Data = [] # soon to replaced with a firestore db
+
+# This class will handle single books
 class book(Resource):
     def get(self,name):
         for x in range(len(Data)):
@@ -24,6 +26,7 @@ class book(Resource):
                 return "Successfully Deleted", 200
         return "No Such Item", 201
 
+#This class will handle our whole collection of books
 class books(Resource):
     def get(self):
         return Data
@@ -34,8 +37,9 @@ class HelloWorld(Resource):
 
 # api.add_resource(HelloWorld, '/')
 
-api.add_resource(books, '/')
-api.add_resource(book, '/Name/<string:name>')
+api.add_resource(books, '/') #endpoint for all books
+api.add_resource(book, '/Name/<string:name>') #endpoint for single books.
+#more endpoints looding...
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
