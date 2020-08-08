7c71423a7f006a6a045280dcd0d8be449e6b0834
MohauNkepane <45736432+MohauNkepane@users.noreply.github.com>
2020-08-08 15:47:11 +0200

    poems post method to firestore init

--- myApi1.py
+++ myApi1.py
@@ -1,5 +1,14 @@
 from flask import Flask,request
 from flask_restful import Resource, Api
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+
+cred = credentials.Certificate("creative-explosion-firebase-adminsdk-3tjnu-313071f290.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 api = Api(app)
@@ -27,6 +36,56 @@ class book(Resource):
         return "No Such Item", 201
 
 #This class will handle our whole collection of books
+class users(Resource):
+    def get(self):
+        return {"Msg":"Users"}
+class poems(Resource):
+
+    def post(self):
+        #Method for posting a single poem to Firestore
+
+        #Get poems data as a request from front-end or postman
+        data = request.json
+        
+        title = data["title"]
+        poem_content = data["poem_content"]
+        rating = data["rating"]
+        no_of_reads = data["no_of_reads"]
+        poet_id = data["poet_id"]
+        likes = data['likes']
+
+        genres = data["genres"]
+        #fetching values from the genres object.
+        ballad =genres["ballad"]
+        elegy=genres["elegy"]
+        #print(elegy)
+
+        #Saving the poem on firestore.
+        doc_ref = db.collection(u'poems').document(u'poem3')
+        doc_ref.set({
+            u'title': u'{}'.format(title),
+            u'poem_content' : u'{}'.format(poem_content),
+            u'rating': rating,
+            u'no_of_reads': no_of_reads,
+            u'poet_id': u'{}'.format(poet_id),
+            u'likes': likes,
+            u'genres': {
+                u'elegy': elegy,
+                u'ballad': ballad
+            }
+        })
+        return {"Msg" : "Stored"}
+
+
+    def get(self):
+        poems_ref = db.collection(u'poems')
+        docs = poems_ref.stream()
+
+
+        for doc in docs:
+            print(f'{doc.id} => {doc.to_dict()}')
+        return {"Msg" : "retrieved"}
+
 class books(Resource):
     def get(self):
         return Data
@@ -39,7 +98,8 @@ class HelloWorld(Resource):
 
 api.add_resource(books, '/') #endpoint for all books
 api.add_resource(book, '/Name/<string:name>') #endpoint for single books.
+api.add_resource(poems, '/poems')
 #more endpoints looding...
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
